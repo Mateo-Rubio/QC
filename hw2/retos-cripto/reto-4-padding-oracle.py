293094dd89f0d795fdf11d6b78d24459278a705e
Mateo Andres Rubio Hernandez <m.rubioh@sis.virtual.uniandes.edu.co>
2026-09-15 17:46:22 -0500

    Nada que funciona!

--- hw2/retos-cripto/reto-4-padding-oracle.py
+++ hw2/retos-cripto/reto-4-padding-oracle.py
@@ -2,7 +2,7 @@ import socket
 s = socket.socket(); s.connect(("32.199.164.87", 1342))
 f = s.makefile("rwb")
 enc = None
-while True:                              # leer el saludo hasta 'LISTO'
+while True:                            
     linea = f.readline()
     print(linea.decode())
     if linea.startswith(b"Cookie cifrada (hex): "):
@@ -13,3 +13,32 @@ while True:                              # leer el saludo hasta 'LISTO'
 def padding_valido(cookie: bytes) -> bool:
     f.write(cookie.hex().encode() + b"\n"); f.flush()
     return b"Padding Invalido" not in f.readline()
+
+cookie = bytearray(enc)
+reves = bytearray()
+for i in range(2):
+    C1 = cookie[i*16: (i+1)*16]
+    cookie_mod = cookie
+    for j in range(15,-1,-1):
+        k = j + 1
+        print("j:",j)
+        while k <= 15:
+            print("k:", k)
+            C1[k] = 15-j
+            k+=1 
+        for byte_value in range(256):
+#           if byte_value == 0 and j == 15: 
+#                continue 
+            C1[j] = byte_value
+            cookie_mod[i*16: (i+1)*16] = C1
+            if padding_valido(bytes(cookie_mod)):
+                print(byte_value)
+                reves.append(byte_value)
+                print(reves)
+                break
+print(reves)
+
+
+
+
+
